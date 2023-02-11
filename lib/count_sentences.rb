f3bd1197a0e3148fd0c644d96e62376c6695b50e
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-11 22:49:24 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?('.') 
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[?.!]/).select{ | word | !word.empty? }.length
   end
 end
+
+puts "HI!".end_with?("!")
+puts "The weather outside is frightful.".sentence?
+puts "This is a string! It has three sentences. Right?".count_sentences
